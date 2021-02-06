a87a6cfa5a41125a6c011002eda5cf29ef4816a0
François Pluchino <francois.pluchino@klipper.dev>
2021-02-06 19:39:40 +0100

    Add retry route for importation

--- Controller/ImportController.php
+++ Controller/ImportController.php
@@ -11,13 +11,17 @@
 
 namespace Klipper\Bundle\ApiImportBundle\Controller;
 
+use Klipper\Bundle\ApiBundle\Action\Upsert;
 use Klipper\Bundle\ApiBundle\Controller\ControllerHelper;
 use Klipper\Component\Content\ContentManagerInterface;
+use Klipper\Component\Import\ImportManagerInterface;
+use Klipper\Component\Import\Message\ImportRunMessage;
 use Klipper\Component\Import\Model\ImportInterface;
 use Klipper\Component\SecurityOauth\Scope\ScopeVote;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -25,6 +29,39 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ImportController
 {
+    /**
+     * @Entity(
+     *     "id",
+     *     class="App:Import"
+     * )
+     *
+     * @Route("/imports/{id}/retry", methods={"PUT"})
+     *
+     * @Security("is_granted('perm:update', 'App\\Entity\\Import')")
+     * @Security("is_granted('perm:import')")
+     */
+    public function retry(
+        ControllerHelper $helper,
+        MessageBusInterface $messageBus,
+        ImportManagerInterface $importManager,
+        ImportInterface $id
+    ): Response {
+        if (class_exists(ScopeVote::class)) {
+            $helper->denyAccessUnlessGranted(new ScopeVote(['meta/import', 'meta/import.readonly'], false));
+        }
+
+        if (!$importManager->reset($id)) {
+            return $helper->view($id);
+        }
+
+        $action = Upsert::build('', $id)->setProcessForm(false);
+        $res = $helper->upsert($action);
+
+        $messageBus->dispatch(new ImportRunMessage($id->getId()));
+
+        return $res;
+    }
+
     /**
      * @Entity(
      *     "id",
